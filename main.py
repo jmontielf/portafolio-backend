e0fb1a75038f0cfb9b5e7d9887bf59c2a633c917
jmontielf <julio.montiel.f@gmail.com>
2020-10-05 19:13:36 -0300

    Added JWT initilization to the app

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #* Main import of libraries
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 #* Import all the routes of the app
 from app.routes.routes import applicationRoutes
@@ -7,6 +8,11 @@ from app.routes.routes import applicationRoutes
 #* Create the instance of the app and pass the intance to the components of the app
 app = Flask(__name__)
 applicationRoutes(app)
+
+#* Setup the Flask-JWT-Extended extension
+#TODO: move the key of JWT to a secret file
+app.config['JWT_SECRET_KEY'] = 'portfvirtual2020'  # Change this!
+jwt = JWTManager(app)
 
 #* Run the app server 
 app.run(host='0.0.0.0')
